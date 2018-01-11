b81dad01144db95c60d28135b73b7f96637933fb
Egor Petrov <petrov.bz.molod@gmail.com>
2018-01-11 18:50:12 +0300

    Init commit

---
+++ Notes/Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Egor Petrov on 11/01/2018.
+//  Copyright © 2018 Egor Petrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
