96eeb95a0f58570293e070f680b2395c10bff92d
Egor Petrov <petrov.bz.molod@gmail.com>
2018-01-18 11:53:27 +0300

    Created cell and reusable extension

---
+++ Notes/Classes/Presentation/WatchingAllNotesStory/ShowAllNotesModule/Subviews/NoteTableViewCell/NoteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoteTableViewCell.swift
+//  Notes
+//
+//  Created by Egor Petrov on 18/01/2018.
+//  Copyright © 2018 Egor Petrov. All rights reserved.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
